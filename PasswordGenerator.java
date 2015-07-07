54e3e459f73cd55ac4f2c009d20152ef8232833b
Brendan Davy <dakaraiprojects@gmail.com>
2015-07-07 00:55:16 -0700

    fixed a bunch
    added an extra method and made it so it all works!!!

--- PasswordGenerator.java
+++ PasswordGenerator.java
@@ -4,6 +4,7 @@ import java.util.InputMismatchException;
 import java.util.NoSuchElementException;
 import java.util.Random;
 import java.util.Scanner;
+import java.util.Stack;
 
 import javax.annotation.Generated;
 
@@ -17,14 +18,13 @@ public class PasswordGenerator {
 		//ask about password parameters
 			//length
 				int length = getLength();
-				//odd/even length check
 			//types of characters used (numbers, caps, specials)
-				//boolean upperCase = askUpperCase();
-				//boolean numbers = askNumbers();
-				//boolean specials = askSpecials();
+				boolean upperCase = askUpperCase();
+				boolean numbers = askNumbers();
+				boolean specials = askSpecials();
 				//make sure at least one type is used
 			//palindrome usage?
-				//boolean palindrome = askPalindrome();
+				boolean palindrome = askPalindrome();
 				//if yes, stick specials at the end?
 				//boolean stickAtEnd = false;
 				//if (palindrome && specials){
@@ -37,13 +37,10 @@ public class PasswordGenerator {
 				//}
 		//create password
 			char[] password = new char[length /*+ addAtEnd*/];
-			/*if (palindrome && !stickAtEnd){
-				//make array password.length-add / 2 filled, then mirror
-			}else if (palindrome){
-				//make array length-add / 2 filled, then mirror and add specials at end
-			}else{
-				*///easist. make array then random, depending on chars used
-				generatePassword(password/*, upperCase, numbers, specials*/);
+			//if (palindrome && stickAtEnd){
+				//
+			//}else{
+				generatePassword(password, palindrome, specials, upperCase, numbers);
 			//}
 			
 		//display password
@@ -54,25 +51,23 @@ public class PasswordGenerator {
 	}
 	
 	public static int getLength(){
-		System.out.print("How many characters long would you like the password to be? ");
+		System.out.println("How many characters long would you like the password to be? ");
 		Scanner console = new Scanner (System.in);
 		try{
 			int length = console.nextInt();
-			console.close();
 			return length;
 		}
 		catch(InputMismatchException exception){
 			System.out.print("That's not a number. Please enter your desired length using numerals. ");
-			
+			System.out.println();
 			return getLength();
 		}
 	}
 	
-	/* public static boolean askUpperCase() throws NoSuchElementException{
-		System.out.print("Would you like this password to contain upper-case characters? (Y/N) ");
+	public static boolean askUpperCase() throws NoSuchElementException{
+		System.out.println("Would you like this password to contain upper-case characters? (Y/N) ");
 		Scanner console = new Scanner (System.in);
 		char answer = Character.toUpperCase(console.next().charAt(0));
-		console.close();
 		
 		if (answer == 'Y'){
 			return true;
@@ -80,15 +75,15 @@ public class PasswordGenerator {
 			return false;
 		}else{
 			System.out.print("Nice try. Please give a valid answer (Y or N)");
+			System.out.println();
 			return askUpperCase();
 		}
 	}
 	
 	public static boolean askNumbers() throws NoSuchElementException{
-		System.out.print("Would you like this password to contain numbers? (Y/N) ");
+		System.out.println("Would you like this password to contain numbers? (Y/N) ");
 		Scanner console = new Scanner (System.in);
 		char answer = Character.toUpperCase(console.next().charAt(0));
-		console.close();
 		
 		if (answer == 'Y'){
 			return true;
@@ -96,15 +91,15 @@ public class PasswordGenerator {
 			return false;
 		}else{
 			System.out.print("Nice try. Please give a valid answer (Y or N)");
+			System.out.println();
 			return askNumbers();
 		}
 	}
 	
 	public static boolean askSpecials() throws NoSuchElementException{
-		System.out.print("Would you like this password to special characters? (Y/N) ");
+		System.out.println("Would you like this password to special characters? (Y/N) ");
 		Scanner console = new Scanner (System.in);
 		char answer = Character.toUpperCase(console.next().charAt(0));
-		console.close();
 		
 		if (answer == 'Y'){
 			return true;
@@ -112,15 +107,15 @@ public class PasswordGenerator {
 			return false;
 		}else{
 			System.out.print("Nice try. Please give a valid answer (Y or N)");
+			System.out.println();
 			return askSpecials();
 		}
 	}
 	
-	public static boolean askPalindrome() throws NoSuchElementException{
-		System.out.print("Would you like this password to contain upper-case characters? (Y/N) ");
+	public static boolean askPalindrome(){
+		System.out.println("Would you like this password use a palindrome format? (Y/N) ");
 		Scanner console = new Scanner (System.in);
 		char answer = Character.toUpperCase(console.next().charAt(0));
-		console.close();
 		
 		if (answer == 'Y'){
 			return true;
@@ -128,15 +123,15 @@ public class PasswordGenerator {
 			return false;
 		}else{
 			System.out.print("Nice try. Please give a valid answer (Y or N)");
+			System.out.println();
 			return askPalindrome();
 		}
 	}
 	
-	public static boolean askAtEnd() throws NoSuchElementException{
-		System.out.print("Would you like the special characters to be placed at the end of the palindrome? (Y/N) ");
+	/*public static boolean askAtEnd() throws NoSuchElementException{
+		System.out.println("Would you like the special characters to be placed at the end of the palindrome? (Y/N) ");
 		Scanner console = new Scanner (System.in);
 		char answer = Character.toUpperCase(console.next().charAt(0));
-		console.close();
 		
 		if (answer == 'Y'){
 			return true;
@@ -144,32 +139,70 @@ public class PasswordGenerator {
 			return false;
 		}else{
 			System.out.print("Nice try. Please give a valid answer (Y or N)");
+			System.out.println();
 			return askAtEnd();
 		}
-	}
+	} */
 	
-	public static int askNumberForEnd() throws NoSuchElementException{
-		System.out.print("How many special characters would you like added to the end? ");
+	/*public static int askNumberForEnd() throws NoSuchElementException{
+		System.out.println("How many special characters would you like added to the end? ");
 		Scanner console = new Scanner (System.in);
 		try{
 			int length = console.nextInt();
-			console.close();
 			return length;
 		}
 		catch(InputMismatchException exception){
 			System.out.print("That's not a number. Please enter your number using numerals. ");
-			
+			System.out.println();
 			return askNumberForEnd();
 		}
 	} */
-	
-	public static void generatePassword(char[] password/*, boolean upperCase, boolean numbers, boolean specials*/){
-		//if (upperCase && numbers && specials){
+
+	public static void generatePassword(char[] password, boolean palindrome, boolean specials, boolean upperCase, boolean numbers){
+		SecureRandom rand = new SecureRandom();
+		if (palindrome){
+			int length = password.length;
+			Stack<Character> otherHalf = new Stack<Character>();
+			int i = 0;
+			while (i < length / 2){
+				char placement = (char) randomSequence(specials, upperCase, numbers);
+				password[i] = placement;
+				otherHalf.push(placement);
+				i++;
+			}
+			if (length % 2 == 1){
+				otherHalf.push((char) randomSequence(specials, upperCase, numbers));
+			}
+			while (!otherHalf.isEmpty()){
+				password[i] = (char) otherHalf.pop();
+				i++;
+			}
+		}else{
 			for (int i = 0; i < password.length; i++){
-				SecureRandom rand = new SecureRandom();
-				char placement = (char) (rand.nextInt(94) + 33);
+				char placement = (char) randomSequence(specials, upperCase, numbers);
 				password[i] = placement;
 			}
-		//}
+		}
+	}
+	
+	public static int randomSequence(boolean specials, boolean upperCase, boolean numbers){
+		SecureRandom rand = new SecureRandom();
+		int placement = (rand.nextInt(94) + 33);
+		if (!specials){
+			if (placement >= 33 && placement <= 47 || placement >=58 && placement <= 64 || placement >= 91 && placement <= 96 || placement >= 123 && placement <= 126){
+				return randomSequence(specials, upperCase, numbers);
+			}
+		}
+		if (!upperCase){
+			if (placement >= 65 && placement <= 90){
+				return randomSequence(specials, upperCase, numbers);
+			}
+		}
+		if (!numbers){
+			if (placement >= 48 && placement <= 57){
+				return randomSequence(specials, upperCase, numbers);
+			}
+		}
+		return placement;
 	}
 }
--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # Password-Generator
 A Java password creator with a variety of options.
+
+Combinations of characters used:
+
+1.	 All Characters (Uppercase, Numbers, Specials)
+1.	 Uppercase, Specials
+1.	 Uppercase Numbers
+1.	 Numbers Specials
+1.	 None (Lowercase Only)
